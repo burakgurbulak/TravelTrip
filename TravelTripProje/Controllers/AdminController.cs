8bcea8fd8c93b19bde7d76d33e8f054bdfdd733e
burakgurbulak <burakgurbulak13@hotmail.com>
2021-11-05 14:19:36 +0300

    Initial commit.

---
+++ TravelTripProje/Controllers/AdminController.cs
@@ -0,0 +1,88 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TravelTripProje.Models.Siniflar;
+
+namespace TravelTripProje.Controllers
+{
+    public class AdminController : Controller
+    {
+        Context c = new Context();
+        // GET: Admin
+        [Authorize]
+        public ActionResult Index()
+        {
+            var degerler = c.Blogs.ToList();
+            return View(degerler);
+        }
+        [HttpGet]
+        public ActionResult YeniBlog()
+        {
+                return View();
+        }
+         [HttpPost]
+        public ActionResult YeniBlog(Blog p)
+        {
+            c.Blogs.Add(p);
+            c.SaveChanges();
+                return RedirectToAction("Index");
+            
+        }
+
+        public ActionResult BlogSil(int id)
+        {
+            var b = c.Blogs.Find(id);
+            c.Blogs.Remove(b);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+        public ActionResult BlogGetir(int id) {
+            var blog = c.Blogs.Find(id);
+            return View("BlogGetir",blog);
+
+        }
+        public ActionResult BlogGuncelle(Blog b) {
+
+            var blog = c.Blogs.Find(b.ID);
+            blog.Aciklama = b.Aciklama;
+            blog.Baslik = b.Baslik;
+            blog.BlogImage = b.BlogImage;
+            blog.Tarih = b.Tarih;
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult YorumListesi()
+        {
+            var yorumlar = c.Yorumlars.ToList();
+
+            return View(yorumlar);
+        }
+        public ActionResult YorumSil(int id)
+        {
+            var b = c.Yorumlars.Find(id);
+            c.Yorumlars.Remove(b);
+            c.SaveChanges();
+            return RedirectToAction("YorumListesi");
+
+        }
+        public ActionResult YorumGetir(int id) {
+            var yorum = c.Yorumlars.Find(id);
+            return View("YorumGetir", yorum);
+        }
+        public ActionResult YorumGuncelle(Yorumlar y)
+        {
+
+            var yorum = c.Yorumlars.Find(y.ID);
+            yorum.KullaniciAdi = y.KullaniciAdi;
+            yorum.Mail = y.Mail; 
+            yorum.Yorum = y.Yorum;
+
+            c.SaveChanges();
+            return RedirectToAction("YorumListesi");
+        }
+    }
+}
---
+++ TravelTripProje/Controllers/BlogController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TravelTripProje.Models.Siniflar;
+
+namespace TravelTripProje.Controllers
+{
+    public class BlogController : Controller
+    {
+        BlogYorum by = new BlogYorum();
+        // GET: Blog
+        Context c = new Context();
+
+
+
+        public ActionResult Index()
+        {
+            //var bloglar = c.Blogs.ToList();
+            by.Deger1 = c.Blogs.ToList();
+            by.Deger3 = c.Blogs.OrderByDescending(x => x.ID).Take(3).ToList();
+
+            return View(by);
+        }
+
+
+        public ActionResult BlogDetay(int id) {
+
+            by.Deger1 = c.Blogs.Where(x => x.ID == id).ToList();
+            by.Deger2 = c.Yorumlars.Where(x => x.Blogid == id).ToList();
+
+            //var blogbul = c.Blogs.Where(x=>x.ID==id).ToList();
+
+            return View(by);
+        }
+
+        [HttpGet]
+        public PartialViewResult YorumYap(int id)
+        {
+            ViewBag.deger = id;
+            return PartialView();
+        }
+        [HttpPost]
+        public PartialViewResult YorumYap(Yorumlar b) {
+
+            c.Yorumlars.Add(b);
+            c.SaveChanges();
+            return PartialView();
+
+        }
+    }
+}
